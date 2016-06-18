15f11a3c575be175a33c0b66383c6feef00dfbb3
lindahsu21 <lindahsu@naver.com>
2016-06-18 21:51:11 +0900

    success

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Post.create(post_params)
 
     respond_to do |format|
       if @post.save
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:body)
+      params.require(:post).permit(:body, :image)
     end
 end
